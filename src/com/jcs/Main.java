18c1eeb81e33472d7005e069da71531193b65af8
J-Ibarra <ibarrajj@gmail.com>
2016-08-18 18:35:34 -0430

    http://learnopengl.com/#!Getting-started/Hello-Triangle Render wiht Shader Program

---
+++ src/com/jcs/Main.java
@@ -0,0 +1,203 @@
+package com.jcs;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Main {
+    private boolean running = false;
+
+    int WIDTH = 800;
+    int HEIGHT = 600;
+    // The window handle
+    private long window;
+
+    ShaderProgram shaderProgram;
+
+    int VBO, VAO;
+
+    private void init() throws Exception {
+        shaderProgram = new ShaderProgram();
+        shaderProgram.createVertexShader("shaders/vertex.vs");
+        shaderProgram.createFragmentShader("shaders/fragment.fs");
+        shaderProgram.link();
+
+
+        float[] vertices = new float[]{
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f, 0.5f, 0.0f
+        };
+
+        VAO = glGenVertexArrays();
+        glBindVertexArray(VAO);
+
+        VBO = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        FloatBuffer fb = org.lwjgl.BufferUtils.createFloatBuffer(vertices.length);
+        fb.put(vertices).flip();
+        glBufferData(GL_ARRAY_BUFFER, fb, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+
+    private void update() {
+
+    }
+
+    private void render() {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        shaderProgram.bind();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+
+    }
+
+    private void oneSecond(int ups, int fps) {
+        System.out.println("ups: " + ups + ", " + fps);
+    }
+
+    private void destroy() {
+        shaderProgram.cleanup();
+    }
+
+    private void initGLFW() {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure our window
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Get the resolution of the primary monitor
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        // Center our window
+        glfwSetWindowPos(
+                window,
+                (vidmode.width() - WIDTH) / 2,
+                (vidmode.height() - HEIGHT) / 2
+        );
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync <- ups and fps = 60 / SwapInterval->
+        glfwSwapInterval(2);
+
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+
+
+    private void initCallbacks() {
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true); // We will detect this in our rendering loop
+        });
+    }
+
+    private void loop() throws Exception {
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+
+        init();
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        int ups, fps;
+        ups = fps = 0;
+
+        glfwSetTime(0);
+        double lastTime = glfwGetTime();
+        while (running) {
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+            ups++;
+            update();
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            fps++;
+            render();
+            glfwSwapBuffers(window); // swap the color buffers
+
+
+            if (glfwGetTime() - lastTime > 1) {
+                oneSecond(ups, fps);
+                ups = fps = 0;
+                lastTime = glfwGetTime();
+            }
+
+            if (glfwWindowShouldClose(window)) {
+                running = false;
+            }
+        }
+
+        destroy();
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        try {
+
+            initGLFW();
+            initCallbacks();
+            loop();
+
+            // Free the window callbacks and destroy the window
+            glfwFreeCallbacks(window);
+            glfwDestroyWindow(window);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Terminate GLFW and free the error callback
+            glfwTerminate();
+            glfwSetErrorCallback(null).free();
+        }
+    }
+
+    private void start() {
+        running = true;
+        run();
+    }
+
+    public static void main(String[] args) {
+        new Main().start();
+    }
+}
